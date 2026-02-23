45c7ec925d32bbe5acd93c1b79d3ecea8726fa04
Priyanka Vikas Bhor <125297313+Priya23021993@users.noreply.github.com>
2026-02-23 22:35:00 +0530

    Add files via upload

---
+++ Assignment/Assignment_51/program5.cpp
@@ -0,0 +1,51 @@
+/* Write generic program to accept N values and reverse the
+   contents.
+
+   Input  :  10   20   30  10   30   40   10   40   10
+
+   Output : 10    40   10   40  30   10   30   20   10
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+
+void Reverse(T *arr ,int iSize)
+{
+    int iStart = 0;
+    int iEnd = iSize -1;
+    T temp;
+
+    while(iStart < iEnd)
+    {
+        temp = arr[iStart];
+        arr[iStart] = arr[iEnd];
+        arr[iEnd]= temp;
+
+        iStart++;
+        iEnd--;
+    }
+}
+
+int main()
+{
+   int arr[] ={10,20,30,10,30,40,10,40,10};
+   for(int i=0; i<9; i++)
+   {
+       cout<<arr[i]<<"\t";
+   }
+   cout<<"\n";
+   Reverse(arr, 9);
+
+   for(int i=0;i<9;i++)
+   {
+      cout<<arr[i]<<"\t";
+   }
+
+    return 0;
+}
+
+
